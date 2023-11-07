831d732b8311d948766f14c77fccc68350e052b8
gushteraa <petkozlatilov1234@gmail.com>
2023-11-07 17:50:40 +0200

    break

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,27 @@
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+let userClickedPattern = []
+
+$(".btn").click(function() {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour)
+    
+    console.log(userClickedPattern);
+})
+
+
+function nextSequence(){
+    let randomNumber = Math.floor(Math.random() * 4)
+    let randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+}
+
+$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+audio.play(); 
+
--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,12 @@
-$("h1").css("color","red");
+$("h1").click(function() {
+    $("h1").css("color", "blue")
+})
+
+
+$("button").click(function () {
+    $("h1").css("color","green")
+})
+
+$(document).keypress(function () {
+    $("h1").text(event.key)
+})
