c67b233c0b55fdaf60b4bac1350c66743e3dd78d
gushteraa <petkozlatilov1234@gmail.com>
2023-11-10 19:10:42 +0200

    x

--- Backend/ExpressServer/index.js
+++ Backend/ExpressServer/index.js
@@ -1,11 +1,18 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello")
-}); 
+  res.send("<h1>Hello</h1>")
+});
 
+app.get("/contact", (req, res) => {
+    res.send("Contact me")
+  });
+  
+  
 app.listen(3000, () => {
-    console.log("Server runing on port 3000.");
-})
+  console.log("Server runing on port 3000.");
+});
+
+app.post
